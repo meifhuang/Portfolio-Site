15cf6b13d902cddfdd21bb8443e2b3ed07895e6f
meifhuang <meifhuang5@gmail.com>
2023-02-16 20:37:03 -0500

    Add animations and effects to the page

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-const navItems = document.querySelectorAll("section");
+const sections = document.querySelectorAll(".sections");
+
+window.addEventListener("scroll", () => {
+    let currentSection = "";
+    sections.forEach(function (section) {
+        const sectionTop = section.offsetTop - 55;
+        if (pageYOffset >= sectionTop) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    const navLinks = document.querySelectorAll('.nav-items')
+    navLinks.forEach(function (link) {
+        link.classList.remove("active");
+        link.textContent = link.textContent.replace(/\W/g, "");
+    })
+
+    const currentLink = document.querySelector(`a[href="#${currentSection}"]`)
+    currentLink.classList.add('active');
+    let content = currentLink.textContent;
+    currentLink.textContent = '</' + content + '>'
+})
