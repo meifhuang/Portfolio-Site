1ddc65c2750a28b2df2f2df4092469161be51add
meifhuang <meifhuang5@gmail.com>
2023-04-05 23:06:07 -0400

    Finalize styling

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const sections = document.querySelectorAll(".sections");
 window.addEventListener("scroll", () => {
     let currentSection = "";
     sections.forEach(function (section) {
-        const sectionTop = section.offsetTop - 55;
+        const sectionTop = section.offsetTop - 30;
         if (pageYOffset >= sectionTop) {
             currentSection = section.getAttribute('id');
         }
@@ -29,8 +29,8 @@ const videos = ["./assets/trail-vid.mov", "./assets/membersonly-vid.mov", "./ass
 
 
 for (let i = 0; i < videos.length; i++) {
-    const imgvid = imgvidArray[i]; 
-    const card_img = imgproj[i] 
+    const imgvid = imgvidArray[i];
+    const card_img = imgproj[i]
     const video = document.createElement('video');
     const source = document.createElement('source');
     imgvid.addEventListener('mouseenter', (() => {
@@ -38,15 +38,73 @@ for (let i = 0; i < videos.length; i++) {
         video.classList.add("img-proj")
         source.src = videos[i]
         video.type = 'mov'
-        video.muted = "true" 
+        video.muted = "true"
         video.appendChild(source);
         imgvid.appendChild(video);
-        video.play(); 
-        card_img.remove(); 
+        video.play();
+        card_img.remove();
         video.style.display = 'block';
     }))
-    imgvid.addEventListener('mouseleave', (()=> {
+    imgvid.addEventListener('mouseleave', (() => {
         imgvid.removeChild(video);
-        imgvid.append(card_img); 
+        imgvid.append(card_img);
     }))
+}
+
+
+(function () {
+    emailjs.init("_GmYj6RU5gGejs_s3");
+})();
+
+// const form = document.querySelector("#contact-form-email")
+const form = document.getElementById("contact-form-email")
+const btn = document.getElementById("email-btn")
+btn.addEventListener("click", () => {
+    console.log("Blicked")
+})
+// console.log(btn)
+console.log(form)
+console.log(document.querySelectorAll(".skills-icon"))
+form.addEventListener("submit", e => {
+    console.log("submitted!")
+    if (!form.checkValidity()) {
+        console.log("invalid")
+        e.preventDefault()
+        form.classList.add('was-validated')
+    }
+    else {
+        e.preventDefault()
+        sendEmail()
+        form.classList.remove('was-validated')
+    }
+
+})
+
+const feedback = document.querySelector(".feedback");
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+console.log(feedback)
+
+function sendEmail() {
+    console.log("sent email")
+    let params = {
+        user_name: document.getElementById("name").value,
+        user_email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    }
+
+    console.log(params.name, params.email, params.message)
+
+    emailjs.send("service_r8cfv3a", "template_fx0iiyv", params)
+        .then(function (response) {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "",
+                document.getElementById("message").value = "";
+            console.log('SUCCESS!', response.status, response.text);
+            feedback.textContent = "Thank you for sending me a message! I look forward to speaking with you."
+            myInput.focus()
+            setTimeout(() => feedback.textContent = "", 5000)
+        }, function (error) {
+            console.log('FAILED...', error);
+        });
 }
